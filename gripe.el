c00ef6cb6731bc901b82c57c27351fd6698508b8
Rafael Nicdao <nicdaoraf@gmail.com>
2021-02-04 23:38:32 +1100

    Use async-shell-command-to-string instead of async-shell-command
    See https://stackoverflow.com/a/23078813
    This is so that we can conveniently attach a callback

--- gripe.el
+++ gripe.el
@@ -19,6 +19,31 @@
 ;;
 ;;; Code:
 
+;; Taken from https://stackoverflow.com/a/23078813
+(defun async-shell-command-to-string (command callback)
+  "Execute shell command COMMAND asynchronously in the background.
+
+Return the temporary output buffer which command is writing to
+during execution.
+
+When the command is finished, call CALLBACK with the resulting
+output as a string."
+  (lexical-let
+      ((output-buffer (generate-new-buffer " *temp*"))
+       (callback-fun callback))
+    (set-process-sentinel
+     (start-process "Shell" output-buffer shell-file-name shell-command-switch command)
+     (lambda (process signal)
+       (when (memq (process-status process) '(exit signal))
+         (with-current-buffer output-buffer
+           (let ((output-string
+                  (buffer-substring-no-properties
+                   (point-min)
+                   (point-max))))
+             (funcall callback-fun output-string)))
+         (kill-buffer output-buffer))))
+    output-buffer))
+
 (defun gripe-find (file-or-dir-path pattern)
   "Find occurrences of a pattern within a file/directory.
 
@@ -27,10 +52,12 @@
   (interactive
    (list (read-file-name "File/directory to search: ")
          (read-string "Pattern to search: ")))
-  (let ((buffer (generate-new-buffer "*gripe-output*")))
-    (async-shell-command (concat "grape --unindent '" pattern "' "  file-or-dir-path)
-                         "*gripe-output*"
-                         "*gripe-error*")))
+  (async-shell-command-to-string (concat "grape --unindent '" pattern "' "  file-or-dir-path)
+                          (lambda (output)
+                            (set-buffer (get-buffer-create "*gripe-output*"))
+                            (erase-buffer)
+                            (insert output)
+                            (display-buffer "*gripe-output*" t))))
 
 (provide 'gripe)
 ;;; gripe.el ends here
